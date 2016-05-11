af959ad234c39a42c33530f2af83a99024555555
Lars Winderling <lars.winderling@performance-advertising.de>
2016-05-11 10:27:45 +0200

    add docs to Tuple

--- src/main/java/com/github/kahalemakai/tuples/Tuple.java
+++ src/main/java/com/github/kahalemakai/tuples/Tuple.java
@@ -2,27 +2,48 @@ package com.github.kahalemakai.tuples;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
 
 /**
- *
- * Created by lars on 14.04.16.
+ * A typed tuple class.
  */
-@RequiredArgsConstructor
 @EqualsAndHashCode
 @Accessors(fluent = true)
-@Getter
 public class Tuple<T, U> {
+    /**
+     * Get the first element of the tuple.
+     *
+     * @return the first element
+     */
+    @Getter
     final private T first;
+
+    /**
+     * Get the second element of the tuple.
+     *
+     * @return the second element
+     */
+    @Getter
     final private U last;
+
+    private Tuple(T first, U last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    /**
+     * Return a new tuple instance.
+     *
+     * @param first first tuple element
+     * @param last second tuple element
+     */
+    public static <S, W> Tuple<S, W> of(final S first, final W last) {
+        return new Tuple<>(first, last);
+    }
 
     @Override
     public String toString() {
         return String.format("(%s, %s)", first, last);
     }
 
-    public static <S, W> Tuple<S, W> of(final S first, final W last) {
-        return new Tuple<>(first, last);
-    }
 }
